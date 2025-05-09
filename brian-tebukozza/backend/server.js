13cb74ce9d0b32c5b91b99225e9c7bdd40861938
brian tebukozza <brintebrn@gmail.com>
2025-05-09 12:21:26 +0300

    my ecommerce 1st commit

---
+++ brian-tebukozza/backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const connectDB = require('./db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+connectDB();
+
+app.listen(8000, () => console.log('Server started on port 4000'));
